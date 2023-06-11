8c8897819847f9a97b6896b5d7f8948d7fe9c68d
Pablo Pérez Martín <aperezvic21@gmail.com>
2023-06-11 20:21:43 +0200

    First commit (Working 7-segment display and button)

---
+++ sketch_jun10a/sketch_jun10a.ino
@@ -0,0 +1,84 @@
+#include "SevSeg.h"
+
+const int MAX_POTENTIOMETER_VAL = 1023;
+
+SevSeg sevseg; 
+
+bool isOnHold = false;
+
+int displayNumber = 0;
+bool halfHourDisplay = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  byte numDigits = 1;
+  byte digitPins[] = {};
+  byte segmentPins[] = {6, 5, 2, 3, 4, 7, 8};
+  bool resistorsOnSegments = true;
+
+  byte hardwareConfig = COMMON_CATHODE; 
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  sevseg.setBrightness(100);
+
+  // punto decimal
+  pinMode(9, OUTPUT); 
+  //BUTTON
+  pinMode(10,INPUT_PULLUP);
+  //BUZZER
+  pinMode(11,OUTPUT);
+  digitalWrite(11, LOW);
+
+}
+
+void loop() {
+
+  if (isOnHold){
+
+  }else{
+      
+
+    readPotentiometer();
+    //Display
+
+    sevseg.setNumber(displayNumber);
+
+    if(halfHourDisplay) digitalWrite(9, HIGH);
+    else digitalWrite(9, LOW);
+
+    sevseg.refreshDisplay();    
+
+    byte buttonState = digitalRead(10);
+    
+    if (buttonState == LOW) {
+        Serial.println("Button is pressed");
+        digitalWrite(11, HIGH);
+    }
+    else {
+        Serial.println("Button is not pressed");
+        digitalWrite(11, LOW);
+    }
+  }
+
+  
+
+  delay(10);  // delay in between reads for stability
+}
+
+
+void readPotentiometer(){
+
+  // read the input on analog pin 0:
+    int sensorValue = analogRead(A0);
+
+    //to get a value from 0.0 - 9.5
+    float normalized_value = ((float) sensorValue / (float) MAX_POTENTIOMETER_VAL) * 9.5; 
+
+    //get only the part before decimals
+    displayNumber = trunc(normalized_value);
+    //get the decimal part and get if is greater that 0.5, to check it as a + half hour
+    halfHourDisplay = normalized_value - int(normalized_value) >= 0.5;
+
+    //Serial.println(String(normalized_value) + " Display: " + displayNumber);
+}
